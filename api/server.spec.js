287638b57bb26dd7f24f754976b16d3f4bdf4c9c
Christopher Shockley <cishockleysr@gmail.com>
2020-02-24 21:17:09 -0500

    all test working

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,19 +1,37 @@
-const server = require('./server.js');
-const request = require('supertest');
+const supertest = require('supertest');
+const server = require('../api/server');
+const db = require('../database/dbConfig');
 
-test('/register', async () => {
-  const res = await request(server)
-    .post('/api/auth/register')
-    .send({ username: 'testName', password: 'testPassword' });
-  expect(res.status).toBe(201);
-  expect(res.type).toBe('application/jspn');
+beforeEach(async () => {
+  await db.seed.run;
+  await db('users').truncate();
 });
 
-describe('/login', () => {
-  test('/login', async () => {
+// test('welcome route', async () => {
+//   const res = await supertest(server).get('/');
+//   expect(res.status).toBe(200);
+//   expect(res.type).toBe('text/html');
+//   expect(res.text).toBe("<h2>Your dad isn't as funny as mine</h2>");
+// });
+
+test('register user route', async () => {
+  const res = await supertest(server)
+    .post('/api/auth/register')
+    .send({ username: 'testUser', password: 'testPassword' });
+  expect(res.status).toBe(201);
+  expect(res.type).toBe('application/json');
+});
+
+describe('testing login', () => {
+  test('login route', async () => {
+    const regRes = await supertest(server)
+      .post('/api/auth/register')
+      .send({ username: 'testUserLogin', password: 'testPassword' });
+    expect(regRes.status).toBe(201);
+    expect(regRes.type).toBe('application/json');
     const res = await supertest(server)
       .post('/api/auth/login')
-      .send({ username: 'testName', password: 'testPassword' });
+      .send({ username: 'testUserLogin', password: 'testPassword' });
     expect(res.status).toBe(200);
     expect(res.type).toBe('application/json');
   });
